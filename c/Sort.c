d901401f2391fcee845c821750bb8fb8004a9d90
Zxiaoxue <18829897183@163.com>
2016-06-15 23:07:20 +0800

    first commit

--- c/Sort.c
+++ c/Sort.c
@@ -4,108 +4,108 @@
 #include <assert.h>
 
 
-//void Insertsort(int *a,size_t size)
-//{
-//	assert(a);
-//	int i = 0;
-//	int j = 0;
-//	int tmp = 0;
-//	for (i = 0; i < size; ++i)
-//	{
-//		j = i;
-//		tmp = a[j + 1];
-//		while (j >= 0 && a[j] < tmp)
-//		{
-//			a[j + 1] = a[j];
-//			j--;
-//		}
-//		a[j+1] = tmp;
-//	}
-//
-//}
+void Insertsort(int *a,size_t size)
+{
+	assert(a);
+	int i = 0;
+	int j = 0;
+	int tmp = 0;
+	for (i = 0; i < size; ++i)
+	{
+		j = i;
+		tmp = a[j + 1];
+		while (j >= 0 && a[j] < tmp)
+		{
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j+1] = tmp;
+	}
 
-//void BubbleSort(int *a,size_t size)
-//{
-//	int i = 0;
-//	int j = 0;
-//	int tmp = 0;
-//	int flag = 0;
-//	assert(a);
-//	for (i = 0; i < size - 1; i++)
-//	{
-//		flag = 0;
-//		for (j = 0; j < size - i - 1; j++)
-//		{
-//			if (a[j]>a[j + 1])
-//			{
-//				tmp = a[j];
-//				a[j] = a[j + 1];
-//				a[j + 1] = tmp;
-//				flag = 1;
-//			}
-//		}
-//		if (flag == 0)
-//		{
-//			return;
-//		}
-//	}
-//}
+}
 
-//void BubbleSort(int *a, size_t size)
-//{
-//		int i = 0;
-//		int j = 0;
-//		int tmp = 0;
-//		int flag = 0;
-//		int pos = 0;
-//		int k = size-1;
-//		assert(a);
-//		for (i = 0; i < size - 1; i++)
-//		{
-//			flag = 0;
-//			for (j = 0; j < k; j++)
-//			{
-//				if (a[j]>a[j + 1])
-//				{
-//					tmp = a[j];
-//					a[j] = a[j + 1];
-//					a[j + 1] = tmp;
-//					pos = j;
-//					flag = 1;
-//				}
-//			}
-//			k = pos;
-//			if (flag == 0)
-//			{
-//				return;
-//			}
-//		}
-//	}
-//
+void BubbleSort(int *a,size_t size)
+{
+	int i = 0;
+	int j = 0;
+	int tmp = 0;
+	int flag = 0;
+	assert(a);
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (a[j]>a[j + 1])
+			{
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return;
+		}
+	}
+}
 
-//void SelectSort(int *a, size_t size)
-//{
-//	int i = 0;
-//	int j = 0;
-//	int tmp = 0;
-//	assert(a);
-//	int min = 0;
-//	for (i = 0; i < size ;i++)
-//	{
-//		min = i;
-//		for (j = i; j < size; j++)
-//		{
-//			if (a[min] > a[j])
-//			{
-//				min = j;
-//			}
-//		}
-//		tmp = a[min];
-//		a[min] = a[i];
-//		a[i] = tmp;
-//
-//	}
-//}
+void BubbleSort(int *a, size_t size)
+{
+		int i = 0;
+		int j = 0;
+		int tmp = 0;
+		int flag = 0;
+		int pos = 0;
+		int k = size-1;
+		assert(a);
+		for (i = 0; i < size - 1; i++)
+		{
+			flag = 0;
+			for (j = 0; j < k; j++)
+			{
+				if (a[j]>a[j + 1])
+				{
+					tmp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = tmp;
+					pos = j;
+					flag = 1;
+				}
+			}
+			k = pos;
+			if (flag == 0)
+			{
+				return;
+			}
+		}
+	}
+
+
+void SelectSort(int *a, size_t size)
+{
+	int i = 0;
+	int j = 0;
+	int tmp = 0;
+	assert(a);
+	int min = 0;
+	for (i = 0; i < size ;i++)
+	{
+		min = i;
+		for (j = i; j < size; j++)
+		{
+			if (a[min] > a[j])
+			{
+				min = j;
+			}
+		}
+		tmp = a[min];
+		a[min] = a[i];
+		a[i] = tmp;
+
+	}
+}
 
 void swap(int *a, int *b)
 {
@@ -115,51 +115,6 @@ void swap(int *a, int *b)
 	*b = tmp;
 }
 
-int particular(int *a, int left, int right)
-{
-	int key = a[left];
-	int begin = left;
-	int end = right - 1;
-	int tmp = 0;
-	assert(a);
-	
-
-	while (begin < end)
-	{
-		while (begin < end&&a[begin] < key)
-		{
-			begin++;
-		}
-		while (begin<end&&a[end]>key)
-		{
-			end--;
-		}
-		if (begin < end)
-		{
-			swap(&a[begin], &a[end]);
-		}
-	
-	}
-	if (a[begin]>a[end])
-		{
-			swap(&a[begin], &key);
-		}
-	return begin;
-
-}
-
-void QuickSort(int *a, int left, int right)
-{
-	int div = 0;
-	assert(a);
-	if (left >= right)
-	{
-		return;
-	}
-	 div = particular(a, left, right);
-	QuickSort(a, 0, div);
-	QuickSort(a, div+1, right);
-}
 
 
 void SelectSort(int *a, size_t size)
