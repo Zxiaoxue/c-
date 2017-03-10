cd19fda57d73af44230f44115f88c506faa1700f
Zxiaoxue <18829897183@163.com>
2017-03-10 22:41:17 +0800

    first commit

---
+++ c/17.3.9复习/17.3.9复习/Test.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//int main()
+//{
+//	int year = 1000;
+//	for(; year < 2000; year++)
+//	{
+//		if(year%4==0&&year%100!=0 ||year%400==0)
+//			printf("%dyear is leap year!\n",year);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//³Ë·¨¿Ú¾÷±í
+//int main()
+//{
+//	for(int i = 1; i<10; i++)
+//	{
+//		for(int j = 1; j<10; j++)
+//		{
+//			printf("%d*%d=%d  ",j,i,i*j);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 100;
+//	for(; i<=200; i++)
+//	{
+//		int j = 1;
+//		int k = sqrt((double)i);
+//
+//		for(j=2; j<k; j++)
+//		{
+//			if(i % j == 0)
+//				break;
+//		}
+//		if(j >= k)
+//		{
+//			printf("%d ",i);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
